cc528fd4aceb421a7546af9a79ef0c90f3937f9f
Cyndy Pantoja <cyndpantoja@gmaiil.com>
2020-05-18 19:24:09 -0500

    diecinueve commit

--- Datos/AlquilerContext.cs
+++ Datos/AlquilerContext.cs
@@ -5,15 +5,17 @@ using Microsoft.EntityFrameworkCore;
 namespace Datos
 {
     public class AlquilerContext : DbContext
-{
-public AlquilerContext(DbContextOptions options) : base(options)
-{
-}
+    {
+        public AlquilerContext(DbContextOptions options) : base(options)
+        {
+        }
 
-//public DbSet<Cliente> Clientes { get; set; }
+        //public DbSet<Cliente> Clientes { get; set; }
 
-// public DbSet<Equipo> Equipos { get; set; }
+        // public DbSet<Equipo> Equipos { get; set; }
 
-public DbSet<Alquiler> Alquilers { get; set; }
-}
+        public DbSet<Alquiler> Alquilers { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Equipo> Esquipos { get; set; }
+    }
 }
--- alquilerTopografia-master/Startup.cs
+++ alquilerTopografia-master/Startup.cs
@@ -37,14 +37,14 @@ namespace alquilerTopografia_master
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
-                    Title = "School API",
-                    Description = "School API - ASP.NET Core Web API",
+                    Title = "AlquilerTopografia-master API",
+                    Description = "AlquilerTopografia-master API - ASP.NET Core Web API",
                     TermsOfService = new Uri("https://cla.dotnetfoundation.org/"),
                     Contact = new OpenApiContact
                     {
-                        Name = "Unicesar",
+                        Name = "Cyndy",
                         Email = string.Empty,
-                        Url = new Uri("https://github.com/borisgr04/CrudNgDotNetCore3"),
+                        Url = new Uri("https://github.com/cpantoja20/AlquilerTopografico-Master-"),
                     },
                     License = new OpenApiLicense
                     {
